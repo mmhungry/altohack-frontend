7b746138808cd6631e5c4328ce7b06cfd7c17124
AltoTechGithub <98011607+AltoTechGithub@users.noreply.github.com>
2023-04-17 02:16:04 +0700

    adding dropdown ui

---
+++ components/Dropdown.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { Button } from "./Button";
+
+interface DropdownProps {
+  options: string[];
+  onSelect: (option: string) => void;
+  button: React.ReactNode;
+}
+
+export const Dropdown: React.FC<DropdownProps> = ({ options, onSelect, button }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleSelect = (option: string) => {
+    setIsOpen(false);
+    onSelect(option);
+  };
+
+  return (
+    <div className="relative inline-block text-left">
+      <div>
+        {React.cloneElement(button as React.ReactElement<any>, {
+          onClick: () => setIsOpen(!isOpen),
+        })}
+      </div>
+      {isOpen && (
+        <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+          <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+            {options.map((option) => (
+              <div
+                key={option}
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                onClick={() => handleSelect(option)}
+              >
+                {option}
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import { NavBar } from '@/components/NavBar';
-
-const Dashboard: React.FC = () => <div>This is the content for dashboard.</div>;
+import React, { useState } from "react";
+import { Card } from "@/components/Card";
+import { Button } from "@/components/Button";
+import { Dropdown } from "@/components/Dropdown";
 
 export default function DashboardPage() {
+  const [selectedTime, setSelectedTime] = useState("Day");
+
+  const handleTimeSelect = (time: string) => {
+    setSelectedTime(time);
+  };
+
+  const timeOptions = ["Day", "Week", "Month", "Year"];
+
   return (
-    <>
-      <Dashboard />
-    </>
+    <div className="grid grid-cols-12 gap-2">
+      <span className="col-span-6">
+        <Card title="Energy Consumption">
+          <span className="flex justify-end">
+            <Dropdown
+              options={timeOptions}
+              onSelect={handleTimeSelect}
+              button={
+                <Button variant="primary" size="lg">
+                  {selectedTime}
+                </Button>
+              }
+            />
+          </span>
+        </Card>
+      </span>
+    </div>
   );
 }
